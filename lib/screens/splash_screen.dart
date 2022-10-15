9616b92accef71d535cf7c5ba6951abc23a0f9e9
Rousticx2022 <104617059+Rousticx2022@users.noreply.github.com>
2022-10-15 19:43:03 +0530

    changes

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -2,8 +2,26 @@ import 'package:flutter/material.dart';
 import 'package:nileur_music_app/screens/login_screen.dart';
 import 'package:get/get.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+  loadApp() {
+    Future.delayed(Duration(seconds: 2), (){
+      Get.to(() => LoginScreen());
+    });
+  }
+
+  @override
+  void initState() {
+    loadApp();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,26 +32,13 @@ class SplashScreen extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            GestureDetector(
-              onTap: ()=> Get.to(LoginScreen()),
-              child: Text(
-                'Nileur',
-                style: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.w900,
-                  color: Color(0xffDEB747),
-                  letterSpacing: 2.0
-                ),
-              ),
+            Text(
+              'Nileur',
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.w900, color: Color(0xffDEB747), letterSpacing: 2.0),
             ),
             Text(
               'The Music App',
-              style: TextStyle(
-                color: Color(0xffB7A3CF),
-                fontSize: 15
-
-              ),
-
+              style: TextStyle(color: Color(0xffB7A3CF), fontSize: 15),
             ),
           ],
         ),
